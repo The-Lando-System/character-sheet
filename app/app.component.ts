0b4620c6c19cb684b72312faa33fb00dc74b804a
Matt <matt.voget@gmail.com>
2017-04-28 19:18:33 -0600

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'character-sheet-app',
+  templateUrl: 'app.component.html',
+  styleUrls: [ 'app.component.css' ],
+  providers: []
+})
+export class AppComponent {
+
+  welcome = 'Matt Voget';
+
+}
